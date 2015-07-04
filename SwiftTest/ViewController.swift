d7ceaafc9b8892e19b2272bcf383670252690530
Strays <878605517@qq.com>
2015-07-04 19:13:51 +0800

    Initial Commit

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Guibin on 15/7/4.
+//  Copyright (c) 2015年 Person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
